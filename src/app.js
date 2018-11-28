5fa7f0e3d62d3d7baa638701e58e69e9690d64c2
fj181435 <kanegawa.masaya@jp.fujitsu.com>
2018-11-28 17:57:53 +0900

    add io

--- src/app.js
+++ src/app.js
@@ -1,12 +1,42 @@
 const http = require("http");
 const fs = require("fs");
+
+let id = 1;
 class Main {
   constructor() {
     const server = http.createServer((request, response) => {
       this.requestHandler(request, response);
     });
     server.listen('4649');
+    this._io = require('socket.io').listen(server);
     console.log('listening');
+    this._garbageRow = [0, 0];//[0, 0, 0, 0];
+    this.ioHandler();
+  }
+
+  ioHandler() {
+    this._io.sockets.on('connection', (socket) => {
+      socket.on('connected', (data) => {
+        this._io.to(socket.id).emit('connected', id++);
+      });
+      //this._io.to(socket.id).emit('connection', id++);
+      // server -> client (自分以外の全員宛に送る)
+      socket.on('attack', (data) => {
+        for (let i in data.rowsNum) {
+          if (0 < this._garbageRow[data.playerId - 1]) {console.log(this._garbageRow[data.playerId - 1]);
+            this._garbageRow[data.playerId - 1]--;
+          } else {
+            for (let j in this._garbageRow) {
+              if (j !== data.playerId - 1) {
+                this._garbageRow[j]++;
+              }
+            }
+          }
+        }
+        this._io.sockets.emit('attack', this._garbageRow);
+        console.log(this._garbageRow);
+      });
+    });
   }
 
   requestHandler(request, response) {
--- src/out.js
+++ src/out.js
@@ -1,7 +1,31 @@
+let url = 'http://10.125.98.153:4649';
+let socket = io.connect(url);
+
+let garbageRows = [];
+let playerId;
+
+socket.on("connected", (id) => {
+	playerId = id;
+	console.log(id);
+});
+
+socket.on("attack", (data) => {
+	for (i in data) {
+		garbageRows[i].innerText = data[i];
+	}
+});
+
 onload = () => {
-	let text = document.getElementById("text");
-	let test = new Test(text);
-	let input = new Input(document, test);
+	garbageRows[0] = document.getElementById("garbage1");
+	garbageRows[1] = document.getElementById("garbage2");
+	socket.emit("connected");
+	//let text = document.getElementById("text");
+	//let test = new Test(text);
+	//let input = new Input(document, test);
+};
+
+document.onkeydown = (e) => {
+	socket.emit("attack", { playerId: playerId, rowsNum: 5 });
 };
 class Input {
   static initPressedTime() {
