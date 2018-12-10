931165549882857f3d68dbd6e9af4c30035dbfb0
fj181435 <kanegawa.masaya@jp.fujitsu.com>
2018-12-10 17:20:05 +0900

    Managering Key

--- src/server/main.js
+++ src/server/main.js
@@ -1,10 +1,39 @@
-const PORT = '4649';
-const io = generateSocketIo();
+class Main extends KeyManager {
+  static _generateSocketIo() {
+    const server = require("http").createServer((req, res) => {
+      Main._reqHandler(req, res);
+    }).listen(Main.PORT);
+    console.log('listening');
+    return require('socket.io').listen(server);
+  }
 
-//let players = [];
-let tetris = new Tetris((data) => io.sockets.emit('updated', data));
+  static _reqHandler(req, res) {
+    //console.log(req.url);
+    if (req.url === '/') {
+      req.url = '/index.html';
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+    } else if (req.url === '/index.css') {
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+    } else if (req.url.match('main')) {
+      req.url = req.url.split('?')[0];
+    }
+    require("fs").readFile('.' + req.url, 'utf-8', (err, data) => {
+      res.end(data);
+    });
+  }
 
-io.sockets.on('connection', (socket) => {
+  constructor() {
+    //let players = [];
+    this._io = Main._generateSocketIo();
+    this._tetris = new Tetris((data) => io.sockets.emit('updated', data));
+  }
+}
+
+Main.PORT = '4649';
+
+let main = new Main();
+
+main._io.on('connection', (socket) => {
   tetris.addPlayer(socket.id, socket.handshake.query['name']);
   console.log('player: ' + socket.id);
 
@@ -24,26 +53,3 @@ io.sockets.on('connection', (socket) => {
   socket.on("rotateRight", () => tetris.onRotateRightKeyPressed(socket.id));
   socket.on("hold", () => /*attack(socket.id, 1)*/tetris.onHoldKeyPressed(socket.id));
 });
-
-function generateSocketIo() {
-  const server = require("http").createServer((request, response) => {
-    requestHandler(request, response);
-  }).listen(PORT);
-  console.log('listening');
-  return require('socket.io').listen(server);
-}
-
-function requestHandler(request, response) {
-  //console.log(request.url);
-  if (request.url === '/') {
-    request.url = '/index.html';
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-  } else if (request.url === '/index.css') {
-    response.writeHead(200, { 'Content-Type': 'text/css' });
-  } else if (request.url.match('main')) {
-    request.url = request.url.split('?')[0];
-  }
-  require("fs").readFile('.' + request.url, 'utf-8', (err, data) => {
-    response.end(data);
-  });
-}
